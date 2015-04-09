1a972796161fb5a5c7bdff7ed37c8f473257258f
Imran Bordiwala <imranbordiwala@IPTARUWKS10.rules.env6.local>
2015-04-09 17:43:18 +0100

    initail commit

---
+++ src/main/java/uk/gov/mvc/AppConfig.java
@@ -0,0 +1,10 @@
+package uk.gov.mvc;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "uk.gov.mvc")
+
+public class AppConfig {
+}
---
+++ src/main/java/uk/gov/mvc/ApplicationController.java
@@ -0,0 +1,17 @@
+package uk.gov.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ApplicationController {
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+    @ResponseBody
+    public String showIndex() {
+        return "Hello world";
+    }
+
+}
